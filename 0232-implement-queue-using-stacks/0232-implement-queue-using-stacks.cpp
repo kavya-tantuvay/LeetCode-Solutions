a5945cdeea0d75b3596ada352ef3460b19bb2b05
kavya-tantuvay <kavyatantuvay@gmail.com>
2025-08-05 22:20:30 +0530

    Time: 0 ms (100%), Space: 9.8 MB (26.75%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -5,29 +5,32 @@ public:
     }
     stack<int>s1,s2;
     void push(int x) {
-        while(s1.size()){
-            s2.push(s1.top());
-            s1.pop();
-        }
         s1.push(x);
-        while(s2.size()){
-            s1.push(s2.top());
-            s2.pop();
-        }
     }
     
     int pop() {
-        int top=s1.top();
-        s1.pop();
+        if(s2.empty()){
+            while(s1.size()){
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+        int top=s2.top();
+        s2.pop();
         return top;
     }
     
     int peek() {
-        return s1.top();
+       if(!s2.empty())return s2.top();
+       while(s1.size()){
+        s2.push(s1.top());
+        s1.pop();
+       }
+       return s2.top();
     }
     
     bool empty() {
-        return s1.empty();
+        return s1.empty() && s2.empty();
     }
 };
 
