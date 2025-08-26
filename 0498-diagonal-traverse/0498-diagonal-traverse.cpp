c616e98b44b372bcacc6d060333ff419cbf902da
kavya-tantuvay <kavyatantuvay@gmail.com>
2025-08-26 11:39:16 +0530

    Time: 37 ms (13.84%), Space: 32.6 MB (9.92%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.cpp
+++ 0498-diagonal-traverse/0498-diagonal-traverse.cpp
@@ -3,21 +3,22 @@ public:
     vector<int> findDiagonalOrder(vector<vector<int>>& matrix) {
         if (matrix.empty() || matrix[0].empty()) return {};
 
-        int m = matrix.size(), n = matrix[0].size();
-        vector<int> result(m * n);
-        int row = 0, col = 0;
-
-        for (int i = 0; i < m * n; i++) {
-            result[i] = matrix[row][col];
-
-            if ((row + col) % 2 == 0) {
-                if (col == n - 1) row++;
-                else if (row == 0) col++;
-                else { row--; col++; }
-            } else {
-                if (row == m - 1) col++;
-                else if (col == 0) row++;
-                else { row++; col--; }
+        int n = matrix.size(), m = matrix[0].size();
+        vector<int> result;
+        unordered_map<int,vector<int>>diag;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                diag[i+j].push_back(matrix[i][j]);
+            }
+        }
+         //No. of diagonals = m+n-1
+        for(int i=0;i<m+n-1;i++){
+            vector<int>list=diag[i];
+            if(i%2==0){
+                reverse(list.begin(),list.end());
+            }
+            for(int i:list){
+                result.push_back(i);
             }
         }
 
