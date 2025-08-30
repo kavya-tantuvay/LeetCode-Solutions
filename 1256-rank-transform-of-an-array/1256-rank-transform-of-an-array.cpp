984803ed07dab132092442d94b850ce329f1a936
kavya-tantuvay <kavyatantuvay@gmail.com>
2025-08-30 19:00:11 +0530

    Time: 32 ms (89.41%), Space: 39.8 MB (84.16%) - LeetHub

--- 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.cpp
+++ 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.cpp
@@ -1,22 +1,25 @@
+using pp=pair<int,int>;
 class Solution {
 public:
     vector<int> arrayRankTransform(vector<int>& arr) {
         if(arr.size()<=0)return {};
-        vector<int>ans(arr);
+      
         int n=arr.size();
-        sort(ans.begin(),ans.end());
-        unordered_map<int,int>rank;
-        rank[ans[0]]=1;
-        for(int i=1;i<n;i++){
-            if(ans[i]==ans[i-1]){
-                rank[ans[i]]=rank[ans[i-1]];
-            }
-            else rank[ans[i]]=rank[ans[i-1]]+1;
-        }
-        vector<int>res;
+        priority_queue<pp,vector<pp>,greater<pp>>minh;
         for(int i=0;i<n;i++){
-            res.push_back(rank[arr[i]]);
+            minh.push({arr[i],i});
         }
-        return res;
+        int rank=0;
+        int prev=INT_MAX;
+        for(int i=0;i<n;i++){
+            auto it=minh.top();
+            minh.pop();
+            if(it.first!=prev){
+                rank++;
+            }
+            prev=it.first;
+            arr[it.second]=rank;
+        }
+        return arr;
     }
 };
