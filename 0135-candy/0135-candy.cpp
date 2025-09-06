1c52e362a0c8584c8d0a882f42b18cf169b10122
kavya-tantuvay <kavyatantuvay@gmail.com>
2025-09-06 22:59:24 +0530

    Time: 0 ms (100%), Space: 22.2 MB (80.25%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int sum=1;
+        int n=ratings.size();
+        if(n == 0) return 0;
+        int i=1;
+     
+        while(i<n){
+            if(ratings[i]==ratings[i-1]){
+                sum+=1;
+                i++;
+                continue;
+            }
+            int peak=1;
+            while(i<n && ratings[i]>ratings[i-1]){
+                peak++;
+                sum+=peak;
+                i++;
+            }
+            int down=0;
+            while(i<n && ratings[i]<ratings[i-1]){
+                down++;
+                sum+=down;
+                i++;
+            }
+            down++;
+            if(down>peak){
+                sum+=down-peak;
+            }
+        }
+        return sum;
+    }
+};
