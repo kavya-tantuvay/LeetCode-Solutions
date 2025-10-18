26c0426f895dc738bb712b89b0a40df865563fce
kavya-tantuvay <kavyatantuvay@gmail.com>
2025-10-18 20:31:01 +0530

    Time: 12 ms (63.42%), Space: 17.4 MB (40.7%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,33 +9,29 @@
  */
 class Solution {
 public:
-    bool dfs(TreeNode*root,vector<TreeNode*>&st,TreeNode*p){
-        if(root==NULL)return false;
-        st.push_back(root);
-        if(root==p){
-            st.push_back(p);
-            return true;
-        }
-        if(dfs(root->left,st,p) || dfs(root->right,st,p))return true;
-        st.pop_back();
-        return false;
-    }
-    vector<TreeNode*>hdfs(TreeNode*root,TreeNode*p){
-        vector<TreeNode*>st;
-        if(root==NULL)return st;
-        dfs(root,st,p);
-        return st;
-    }
+    // bool dfs(TreeNode*root,vector<TreeNode*>&st,TreeNode*p){
+    //     if(root==NULL)return false;
+    //     st.push_back(root);
+    //     if(root==p){
+    //         st.push_back(p);
+    //         return true;
+    //     }
+    //     if(dfs(root->left,st,p) || dfs(root->right,st,p))return true;
+    //     st.pop_back();
+    //     return false;
+    // }
+    // vector<TreeNode*>hdfs(TreeNode*root,TreeNode*p){
+    //     vector<TreeNode*>st;
+    //     if(root==NULL)return st;
+    //     dfs(root,st,p);
+    //     return st;
+   
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        vector<TreeNode*>s1,s2;
-        s1=hdfs(root,p);
-        s2=hdfs(root,q);
-        int n=min(s1.size(),s2.size());
-        TreeNode*lca;
-        for(int i=0;i<n;i++){
-            if(s1[i]==s2[i])lca=s1[i];
-            else break;
-        }
-        return lca;
+       if(root==NULL || root==p || root==q)return root;
+       TreeNode*left=lowestCommonAncestor(root->left,p,q);
+       TreeNode*right=lowestCommonAncestor(root->right,p,q);
+        if(left==NULL)return right;
+        else if(right==NULL)return left;
+        else return root;
     }
 };
