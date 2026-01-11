3dd707dbc0f914eefb054c0526b72d7c17d1b9ab
kavya-tantuvay <kavyatantuvay@gmail.com>
2026-01-11 15:48:50 +0530

    Time: 5 ms (66.95%), Space: 17.9 MB (75.35%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,47 +1,40 @@
 class Solution {
 public:
-    int histogram(vector<int>&nums){
-        stack<int>st;
-        int n=nums.size();
-        int maxi=0;
-        int nse,pse;
-        for(int i=0;i<n;i++){
-            while(!st.empty() && nums[st.top()]>=nums[i]){
-                int element=nums[st.top()];
+    int area(vector<int>& heights) {
+        int n = heights.size();
+        stack<int> st;
+        int maxArea = 0;
+
+        for (int i = 0; i <= n; i++) {
+            int h = (i == n) ? 0 : heights[i];
+            while (!st.empty() && h < heights[st.top()]) {
+                int height = heights[st.top()];
                 st.pop();
-                nse=i;
-                pse=st.empty()?-1:st.top();
-                int area=element*(nse-pse-1);
-                maxi=max(maxi,area);
+                int width = st.empty() ? i : i - st.top() - 1;
+                maxArea = max(maxArea, height * width);
             }
             st.push(i);
         }
-        while(!st.empty()){
-            nse=n;
-            int element=nums[st.top()];
-            st.pop();
-            pse=st.empty()?-1:st.top();
-            int area=element*(nse-pse-1);
-            maxi=max(maxi,area);
-        }
-        return maxi;
+        return maxArea;
     }
+
     int maximalRectangle(vector<vector<char>>& matrix) {
-        int n=matrix.size();
-        int m=matrix[0].size();
-        int maxrec=0;
-        vector<vector<int>>presum(n, vector<int>(m, 0));
-        for(int i=0;i<m;i++){
-            int sum=0;
-            for(int j=0;j<n;j++){
-                sum+=matrix[j][i]-'0';
-                if(matrix[j][i]=='0')sum=0;
-                presum[j][i]=sum;
+        int m = matrix.size();
+        if (m == 0) return 0;
+        int n = matrix[0].size();
+
+        vector<int> hist(n, 0);
+        int ans = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == '1')
+                    hist[j]++;
+                else
+                    hist[j] = 0;
             }
+            ans = max(ans, area(hist));
         }
-        for(int i=0;i<n;i++){
-            maxrec=max(maxrec,histogram(presum[i]));
-        }
-        return maxrec;
+        return ans;
     }
 };
