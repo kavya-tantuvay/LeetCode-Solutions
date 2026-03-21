61e4d3ab98d570f1a02d693d2f7d3444c9794dc6
kavya-tantuvay <kavyatantuvay@gmail.com>
2026-03-21 19:46:54 +0530

    Time: 0 ms (100%), Space: 13.5 MB (22.93%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,14 +1,31 @@
 class Solution {
 public:
-    int minimumTotal(vector<vector<int>>& t) {
-        const int n=t.size();
-        for(int i=1; i<n; i++){
-            t[i][0]+=t[i-1][0];
-            t[i][i]+=t[i-1][i-1];
-            for(int j=1; j<i; j++){
-                t[i][j]+=min(t[i-1][j], t[i-1][j-1]);
+    int path(int r,int c,int n,vector<vector<int>>&dp,vector<vector<int>>& triangle){
+        if(r==n-1)return triangle[r][c];
+        if(dp[r][c]!=-1)return dp[r][c];
+        int down=triangle[r][c]+path(r+1,c,n,dp,triangle);
+        int diag=triangle[r][c]+path(r+1,c+1,n,dp,triangle);
+        return dp[r][c]=min(down,diag);
+    }
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n=triangle.size();
+        int m=triangle[n-1].size();
+        vector<vector<int>> dp = triangle;
+        for(auto &row : dp){
+            for(auto &x : row){
+             x = -1;
             }
         }
-        return *min_element(t[n-1].begin(), t[n-1].end());
+        for(int j=0;j<m;j++){
+            dp[n-1][j]=triangle[n-1][j];
+        }
+        for(int r=n-2;r>=0;r--){
+            for(int c=r;c>=0;c--){
+                int d=triangle[r][c]+dp[r+1][c];
+                int dg=triangle[r][c]+dp[r+1][c+1];
+                dp[r][c]=min(d,dg);
+            }
+        }
+        return dp[0][0];
     }
 };
