e34028cfdda1f82992562ae023d25d6a3d8c3f65
kavya-tantuvay <kavyatantuvay@gmail.com>
2026-04-12 23:36:57 +0530

    Time: 3 ms (89.56%), Space: 9.7 MB (63.86%) - LeetHub

---
+++ 1320-minimum-distance-to-type-a-word-using-two-fingers/1320-minimum-distance-to-type-a-word-using-two-fingers.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int dist(char a, char b) {
+        int x1 = (a - 'A') / 6, y1 = (a - 'A') % 6;
+        int x2 = (b - 'A') / 6, y2 = (b - 'A') % 6;
+        return abs(x1 - x2) + abs(y1 - y2);
+    }
+
+    int minimumDistance(string word) {
+        vector<int> dp(26, 0);
+        int total = 0;
+
+        for (int i = 0; i < word.size() - 1; i++) {
+            char a = word[i], b = word[i + 1];
+            int d = dist(a, b);
+            total += d;
+
+            vector<int> newDp(26, 0);
+
+            for (int j = 0; j < 26; j++) {
+                // same finger
+                newDp[j] = max(newDp[j], dp[j]);
+
+                // use second finger
+                int saving = dp[j] + d - dist('A' + j, b);
+                newDp[a - 'A'] = max(newDp[a - 'A'], saving);
+            }
+
+            dp = newDp;
+        }
+
+        int maxSaving = *max_element(dp.begin(), dp.end());
+        return total - maxSaving;
+    }
+};
