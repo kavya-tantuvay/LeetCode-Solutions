eb6b6400593eff62032dc3dfad4daa4e04b31c81
kavya-tantuvay <kavyatantuvay@gmail.com>
2026-06-09 21:44:00 +0530

    Time: 0 ms (100%), Space: 14.1 MB (75.17%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -8,16 +8,30 @@ public:
         int not_take=f(nums,ind+1,prev,dp);
         return dp[ind][prev+1]=max(take,not_take);
     }
-    int lengthOfLIS(vector<int>& nums) {
-        int n=nums.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
-        for(int ind=n-1;ind>=0;ind--){
-            for(int prev=ind-1;prev>=-1;prev--){
-                int len=dp[ind+1][prev+1];
-                if(prev==-1 || nums[ind]>nums[prev])len=max(len,1+dp[ind+1][ind+1]);
-                dp[ind][prev+1]=len;
+    int lengthOfLIS(vector<int>& arr) {
+        int n=arr.size();
+        // vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        // for(int ind=n-1;ind>=0;ind--){
+        //     for(int prev=ind-1;prev>=-1;prev--){
+        //         int len=dp[ind+1][prev+1];
+        //         if(prev==-1 || nums[ind]>nums[prev])len=max(len,1+dp[ind+1][ind+1]);
+        //         dp[ind][prev+1]=len;
+        //     }
+        // }
+        // return dp[0][0];
+        vector<int>temp;
+        temp.push_back(arr[0]);
+        int len=1;
+        for(int i=1;i<n;i++){
+            if(arr[i]>temp.back()){
+                temp.push_back(arr[i]);
+                len++;
+            }
+            else{
+                int ind=lower_bound(temp.begin(),temp.end(),arr[i])-temp.begin();
+                temp[ind]=arr[i];
             }
         }
-        return dp[0][0];
+        return len;
     }
 };
