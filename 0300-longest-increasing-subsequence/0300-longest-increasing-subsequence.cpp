6a19264b55b9a50ce2bf446eb93344f321ab0e90
kavya-tantuvay <kavyatantuvay@gmail.com>
2026-06-09 15:49:36 +0530

    Time: 275 ms (29.86%), Space: 296.3 MB (25.53%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -10,7 +10,14 @@ public:
     }
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
-        vector<vector<int>>dp(n,vector<int>(n+1,-1));
-        return f(nums,0,-1,dp);
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        for(int ind=n-1;ind>=0;ind--){
+            for(int prev=ind-1;prev>=-1;prev--){
+                int len=dp[ind+1][prev+1];
+                if(prev==-1 || nums[ind]>nums[prev])len=max(len,1+dp[ind+1][ind+1]);
+                dp[ind][prev+1]=len;
+            }
+        }
+        return dp[0][0];
     }
 };
