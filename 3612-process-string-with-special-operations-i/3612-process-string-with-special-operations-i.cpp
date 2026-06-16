90d1afbf5d088b10d30d0ef8cca571269c054aac
kavya-tantuvay <kavyatantuvay@gmail.com>
2026-06-16 17:26:15 +0530

    Time: 1 ms (83.71%), Space: 39.8 MB (69.7%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string processStr(string s) {
+        string temp="";
+        for (char c:s){
+            if(c=='*'){
+                if(temp.length()>=1)temp.pop_back();
+            }
+            else if(c=='#')temp+=temp;
+            else if(c=='%')reverse(temp.begin(),temp.end());
+            else temp+=c;
+        }
+        return temp;
+    }
+};
