34051fa90e7bd9336da5392a9d698cd64a13d055
kavya-tantuvay <kavyatantuvay@gmail.com>
2026-06-16 13:47:29 +0530

    Time: 11 ms (23.38%), Space: 12.6 MB (9.62%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
-    int maxx(vector<int>&nums,int i,int j){
-        int maxi=nums[i];
-        for(int k=i;k<=j;k++){
-            maxi=max(maxi,nums[k]);
-        }
-        return maxi;
-    }
+    
     int f(int i,int n,int k,vector<int>&arr,vector<int>&dp){
         if(i==n)return 0;
         if(dp[i]!=-1)return dp[i];
         int maxi=0;
-        for(int j=i;j<n;j++){
-            if(j-i+1<=k){
-                int r=maxx(arr,i,j);
-                int summ=(j-i+1)*r+f(j+1,n,k,arr,dp);
-                maxi=max(maxi,summ);
-            }
+        int maxans=0;
+        int len=0;
+        for(int j=i;j<min(i+k,n);j++){
+            len++;
+            maxi=max(arr[j],maxi);
+            int summ=len*maxi+f(j+1,n,k,arr,dp);
+            maxans=max(maxans,summ);
+            
         }
-        return dp[i]=maxi;
+        return dp[i]=maxans;
     }
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
         int n=arr.size();
