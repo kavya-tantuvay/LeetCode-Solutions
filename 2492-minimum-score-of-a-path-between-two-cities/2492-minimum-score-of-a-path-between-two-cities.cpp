a4d432860de74f26aede824c6514a63e6d3b0238
kavya-tantuvay <kavyatantuvay@gmail.com>
2026-07-04 09:17:43 +0530

    Time: 4 ms (99.83%), Space: 107.6 MB (93.35%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -33,31 +33,18 @@ class Solution {
 public:
     
     int minScore(int n, vector<vector<int>>& roads) {
-        vector<pair<int,int>>adj[n+1];
-        vector<int>vis(n+1,0);
-        vis[1]=1;
         disjointset ds(n+1);
-        int maxi=0;
-        for(auto it:roads){
-            int u=it[0];
-            int v=it[1];
-            int d=it[2];
-            ds.unionBysize(u,v);
-            adj[u].push_back({v,d});
-            adj[v].push_back({u,d});
-            maxi=max(maxi,d);
+        for (auto &it : roads) {
+            ds.unionBysize(it[0], it[1]);
         }
-        int ans=maxi;
-        for(int i=1;i<=n;i++){
-            for(auto it:adj[i]){
-                int u=it.first;
-                int d=it.second;
-                if(!vis[i]){
-                    if(ds.findUpar(u)==ds.findUpar(n))ans=min(ans,d);
-                }
-            }
-            vis[i]=1;
+
+        int ans = INT_MAX;
+        int root = ds.findUpar(1);
+
+        for (auto &it : roads) {
+            if (ds.findUpar(it[0]) == root)
+                ans = min(ans, it[2]);
         }
         return ans;
-    }
+    }      
 };
