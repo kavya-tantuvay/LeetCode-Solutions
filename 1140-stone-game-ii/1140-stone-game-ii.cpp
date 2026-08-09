f7e58b7f9fcfdb2a678c430c4f99ebef5b1f8a51
kavya-tantuvay <kavyatantuvay@gmail.com>
2026-08-09 13:53:16 +0530

    Time: 122 ms (5.13%), Space: 31 MB (9.78%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.cpp
+++ 1140-stone-game-ii/1140-stone-game-ii.cpp
@@ -3,19 +3,16 @@ public:
     int stoneGameII(vector<int>& piles) {
         int n = piles.size();
 
-        // dp[i][m][0] -> Bob's turn
-        // dp[i][m][1] -> Alice's turn
         vector<vector<vector<int>>> dp(
             n + 1,
             vector<vector<int>>(n + 1, vector<int>(2, 0))
         );
 
-        // i = n is already 0 for both players
 
         for (int i = n - 1; i >= 0; i--) {
             for (int m = n; m >= 1; m--) {
 
-                // Alice's turn -> maximize
+             
                 int aliceRes = 0;
                 int sum = 0;
 
@@ -31,7 +28,7 @@ public:
                 dp[i][m][1] = aliceRes;
 
 
-                // Bob's turn -> minimize
+               
                 int bobRes = INT_MAX;
 
                 for (int x = 1; x <= 2 * m && i + x <= n; x++) {
